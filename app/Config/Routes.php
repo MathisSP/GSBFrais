d1248dac670712dce85367490991eb75e10960e9
Sokona Sidibé <sidibe-s@saint-louis29.net>
2025-11-18 15:02:12 +0100

    pffffffff

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,9 +13,9 @@ $routes->get('/connexion/deconnexion', 'Connexion::deconnexion');
 $routes->get('/accueil', 'Accueil::index');  
 
 $routes->get('gererfrais', 'GererFrais::index');
-$routes->post('gererfrais/maj_fraisforfait', 'GererFrais::valider_maj_fraisforfait');
-$routes->post('gererfrais/creation_fraishorsforfait', 'GererFrais::valider_creation_fraishorsforfait');
-$routes->get('gererfrais/supp_fraishorsforfait/(:num)', 'GererFrais::supprimer_fraishorsforfait/$1');
+$routes->post('gererfrais/maj_fraisforfait', 'Gererfrais::valider_maj_fraisforfait');
+$routes->post('gererfrais/creation_fraishorsforfait', 'Gererfrais::valider_creation_fraishorsforfait');
+$routes->get('gererfrais/supp_fraishorsforfait/(:num)', 'Gererfrais::supprimer_fraishorsforfait/$1');
 
 $routes->get('etatfrais', 'EtatFrais::index');
 $routes->post('etatfrais/mois', 'EtatFrais::selectionner_mois');
